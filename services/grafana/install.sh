b6df4981499067ca677cad1e47cde50b6bdd380e
badele <bruno@adele.im>
2017-11-30 22:48:47 +0100

    Create grafana.db before launching container

--- services/grafana/install.sh
+++ services/grafana/install.sh
@@ -16,6 +16,7 @@ conf=$1
 CAPPNAME="DH_CONF_GRAFANA_${conf^^}_APPNAME" ; APPNAME=${!CAPPNAME}
 NODENAME="${!CAPPNAME}-${conf}"
 CIMGNAME="DH_CONF_GRAFANA_${conf^^}_IMGNAME" ; IMGNAME=${!CIMGNAME}
+CINFLUXDB_SERVER="DH_CONF_GRAFANA_${conf^^}_INFLUXDB_SERVER" ; INFLUXDB_SERVER=${!CINFLUXDB_SERVER}
 
 # Get docker image
 docker pull ${IMGNAME}
@@ -28,6 +29,13 @@ mkdir -p /data/docker/${NODENAME}/dashboards
 # Copy configuration files
 cp $SRC/conf/grafana.ini /data/docker/${NODENAME}/etc/
 cp $SRC/dashboards/*.json /data/docker/${NODENAME}/dashboards
+
+# Generate sqlite grafana.db database
+if [ ! -e /data/docker/${NODENAME}/lib/grafana.db ]; then
+  echo "=> Generate grafana.db"
+  replaceVariablesInFile $SRC/conf/grafana.sql /data/docker/${NODENAME}/lib/grafana.sql
+  docker run -it -v /data/docker/${NODENAME}/lib:/root/db nouchka/sqlite3 grafana.db ".read grafana.sql"
+fi
 
 # Create service
 replaceVariablesInFile $SRC/systemd.service /etc/systemd/system/${NODENAME}.service
