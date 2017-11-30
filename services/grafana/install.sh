90b4341c59872eb2ca30dc212bfbceae6bed3a02
badele <bruno@adele.im>
2017-11-30 08:26:14 +0100

    Add grafana dashboard

--- services/grafana/install.sh
+++ services/grafana/install.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-SRC="`( cd $(dirname \"$0\") && pwd )`"
-source $SRC/db.env
-
+# The script must be running in root
 if [ "$(whoami)" != "root" ] ; then
    echo "Please run in root"
    exit 0
 fi
 
+# Load lib and environment vars
+SRC="`( cd $(dirname \"$0\") && pwd )`"
+source /data/docker/conf/nodes.env
+source $SRC/../../lib.sh
+
+# Init vars
+conf=$1
+CAPPNAME="DH_CONF_GRAFANA_${conf^^}_APPNAME" ; APPNAME=${!CAPPNAME}
+NODENAME="${!CAPPNAME}-${conf}"
+CIMGNAME="DH_CONF_GRAFANA_${conf^^}_IMGNAME" ; IMGNAME=${!CIMGNAME}
+
 # Get docker image
-docker pull $IMGNAME
+docker pull ${IMGNAME}
 
 # Create folder & configuration
-mkdir -p /data/docker/$APPNAME/data
-mkdir -p /data/docker/$APPNAME/conf
-cp $SRC/db.env /data/docker/$APPNAME/conf/
+mkdir -p /data/docker/${NODENAME}/etc
+mkdir -p /data/docker/${NODENAME}/dashboards
+
+# Copy configuration files
+cp $SRC/conf/grafana.ini /data/docker/${NODENAME}/etc/
+cp $SRC/dashboards/*.json /data/docker/${NODENAME}/dashboards
 
 # Create service
-cp $SRC/systemd.service /etc/systemd/system/$APPNAME.service
+replaceVariablesInFile $SRC/systemd.service /etc/systemd/system/${NODENAME}.service
 systemctl daemon-reload
-systemctl enable $APPNAME
-systemctl restart $APPNAME
+systemctl enable ${NODENAME}
+systemctl restart ${NODENAME}
