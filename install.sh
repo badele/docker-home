ec4ac4ba94e0852b87f474a3086cfa08ef8ebfec
badele <bruno@adele.im>
2017-12-30 18:29:12 +0100

    reorganize docker-compose configuration

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # The script must be running in root
@@ -19,15 +20,13 @@ SRC="`( cd $(dirname \"$0\") && pwd )`"
 source ./nodes.env
 source ./lib.sh
 
-# Compute node vars
-mkdir -p /data/docker/conf
-set | egrep "^DH_|DOCKER_HOST" | tee /data/docker/conf/nodes.env
-
 # Search current node installation
+
 selectednode=-1
-for nodeidx in ${!DH_SERVICES[*]}
+for nodeidx in ${!DH_NODENAME[*]}
 do
-  if [ "$DH_MYNODEIP" == "${DH_NODEIP[${nodeidx}]}" ]; then
+  echo "## $(hostname)" == "${DH_NODENAME[${nodeidx}]}"
+  if [ "$(hostname)" == "${DH_NODENAME[${nodeidx}]}" ]; then
     selectednode=$nodeidx
   fi
 done
@@ -37,15 +36,6 @@ if [ $selectednode -eq -1 ];then
   exit 0
 fi
 
-# Install all services for this node (host computer)
-for serviceconf in ${DH_SERVICES[$selectednode]}
-do
-  service=$(echo "$serviceconf" | cut -d':' -f1)
-  conf=$(echo "$serviceconf" | cut -d':' -f2)
-  SRC="`( cd $(dirname \"$0\") && pwd )`"
-
-  print_title "Install $service with '$conf' configuration"
-  if [ -e $SRC/services/$service/install.sh ]; then
-      $SRC/services/$service/install.sh "$conf" "$1"
-  fi
-done
+echo "Verify node${selectednode} docker compose configuration"
+cd node${selectednode}
+docker-compose config
