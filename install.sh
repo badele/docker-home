f0c0910c19abe93953d0d44104576a6d604a011b
badele <bruno@adele.im>
2018-01-02 13:55:09 +0100

    First version with another docker installation method

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+TMPDIR="/tmp/docker-home"
+SRC="`( cd $(dirname \"$0\") && pwd )`"
+
 # The script must be running in root
 if [ "$(whoami)" != "root" ] ; then
    echo "Please run in root"
    exit 0
 fi
 
+# Select docker environment
 if [ "$1" == "" ]; then
     echo "Usage:"
     echo "$0 environment"
@@ -13,18 +17,14 @@ if [ "$1" == "" ]; then
     exit 0
 fi
 
-
-# Load lib and environment vars
-SRC="`( cd $(dirname \"$0\") && pwd )`"
-
 # Export nodes variables
 set -a
 source ./nodes.env $1
 set +a
 
-source ./lib.sh
+#source ./lib.sh
 
-# Search current node installation
+# Search node installation
 selectednode=-1
 for nodeidx in ${!DH_NODENAME[*]}
 do
@@ -32,21 +32,34 @@ do
     selectednode=$nodeidx
   fi
 done
-
 if [ $selectednode -eq -1 ];then
   echo "No install needed here"
   exit 0
 fi
+ROOTNODE=node${selectednode}
 
-# Sync files
-cd node${selectednode}
+# Copy node configurations to temporary file
+mkdir -p "$TMPDIR"
+rsync -avr --delete --exclude docker-compose.yml "${ROOTNODE}/" "${TMPDIR}/${ROOTNODE}"
+
+# Replace all variables
+for filename in $(find "${TMPDIR}/${ROOTNODE}" -type f); do
+	envsubst < "$filename" > "${filename}.bak"
+	mv "${filename}.bak" "${filename}"
+done
+
+# Sync configurations to docker destination
+cd "${TMPDIR}/${ROOTNODE}"
 if [ "$DH_ENV" == "public" ]; then
-	for dirname in $(ls -d */); do
-		dirname=${dirname%%/};
-		rsync -avr --update "${dirname}/" "/data/docker/${dirname}-${DH_ENV}"
-	done
+	cd "${TMPDIR}/${ROOTNODE}"
 fi
-
+for dirname in $(ls -d */); do
+	dirname=${dirname%%/};
+	mkdir -p "/data/docker/${dirname}-${DH_ENV}"
+	rsync -avr --update "${dirname}/" "/data/docker/${dirname}-${DH_ENV}"
+done
 
 # Launching containers
+cd "${SRC}/${ROOTNODE}"
+docker-compose down
 docker-compose up -d
