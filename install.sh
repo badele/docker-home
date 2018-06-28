28dd3b2787a3337984d63d5eddd02bad3e17bf3a
Bruno Adelé <bruno@adele.im>
2018-06-28 08:41:03 +0200

    Fix installation

--- install.sh
+++ install.sh
@@ -1,70 +1,63 @@
-#!/bin/bash
+#!/bin/bash -x
 
 TMPDIR="/tmp/docker-home"
-SRC="`( cd $(dirname \"$0\") && pwd )`"
+SRC="$( (cd $(dirname "$0") && pwd))"
 
 # The script must be running in root
-if [ "$(whoami)" != "root" ] ; then
-   echo "Please run in root"
-   exit 0
+if [ "$(whoami)" != "root" ]; then
+	echo "Please run in root"
+	exit 0
 fi
 
 # Select docker environment
-if [ "$1" == "" ]; then
-    echo "Usage:"
-    echo "$0 environment"
-    echo "ex: $0 private/public"
-    exit 0
+if [ "$2" == "" ]; then
+	echo "Usage:"
+	echo "$0 environment nodename"
+	echo "ex: $0 private/public fullstack"
+	exit 0
+fi
+
+if [ ! -e "$SRC/$2" ]; then
+	echo "$2 node not exist"
+	exit 1
 fi
 
 # Export nodes variables
 set -a
 source ./$1.env $1
 set +a
+export DH_NODE="$2"
 
-#source ./lib.sh
-
-# Search node installation
-selectednode=-1
-for nodeidx in ${!DH_NODENAME[*]}
-do
-  if [ "$(hostname)" == "${DH_NODENAME[${nodeidx}]}" ]; then
-    selectednode=$nodeidx
-  fi
-done
-if [ $selectednode -eq -1 ];then
-  echo "No install needed here"
-  exit 0
+# Root remove protection
+if [ "$(realpath $DH_DSTDOCKER)" = "/" ]; then
+	echo "Cannot store docker images in / folder"
+	exit 1
 fi
-
-# Set Node id
-set -a
-DH_NODE=node${selectednode}
-set +a
 
 # Copy node configurations to temporary file
 TPLDEST="${TMPDIR}/${DH_NODE}/${DH_ENV}"
 mkdir -p "${TPLDEST}"
-rsync -avr --delete --exclude docker-compose.yml --exclude tmp/ "${DH_NODE}/" "${TPLDEST}"
+rsync -avr --delete "${DH_NODE}/" "${TPLDEST}"
 
 # Replace all variables
 for filename in $(find "${TPLDEST}" -type f); do
-	envsubst < "$filename" > "${filename}.bak"
+	envsubst <"$filename" >"${filename}.bak"
 	mv "${filename}.bak" "${filename}"
 done
+cp "${TPLDEST}/docker-compose.yml" "${DH_DSTDOCKER}/${DH_NODE}-${DH_ENV}-docker-compose.yml"
 
 # Sync configurations to docker destination
 cd "${TPLDEST}"
 
-# Docker don"t support ADD/COPY absolute path
+# Docker don't support ADD/COPY absolute path
 for dirname in $(ls -d */); do
-	dirname=${dirname%%/};
-	mkdir -p "$SRC/${DH_NODE}/${dirname}/tmp/${DH_ENV}"
-	rsync -avr --delete "${dirname}/" "$SRC/${DH_NODE}/${dirname}/tmp/${DH_ENV}"
+	dirname=${dirname%%/}
+	#mkdir -p "${DH_DSTDOCKER}/${dirname}-${DH_NODE}-${DH_ENV}"
+	rsync -avr --ignore-existing "${dirname}/" "${DH_DSTDOCKER}/${dirname}-${DH_NODE}-${DH_ENV}"
 done
 
 # Launching containers
-cd "${SRC}/${DH_NODE}"
-docker-compose down
-docker-compose config
-docker-compose up --build zoneminder
+docker-compose -f "${DH_DSTDOCKER}/${DH_NODE}-${DH_ENV}-docker-compose.yml" down
+docker-compose -f "${DH_DSTDOCKER}/${DH_NODE}-${DH_ENV}-docker-compose.yml" config
+#docker-compose -f "${DH_DSTDOCKER}/${DH_NODE}-${DH_ENV}-docker-compose.yml" up --build zoneminder
+docker-compose -f "${DH_DSTDOCKER}/${DH_NODE}-${DH_ENV}-docker-compose.yml" up
